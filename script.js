f865724d3ed89d53c6ded19eb919457f5a0e978f
malik <malikmullar105@gmail.com>
2026-08-27 09:47:13 +0300

    Add silence mode reminder

---
+++ script.js
@@ -0,0 +1,5 @@
+const silenceButton = document.getElementById("silenceButton");
+
+silenceButton.addEventListener("click", function () {
+    alert("🔕 Reminder: Please switch your phone to Silent Mode before entering the Masjid.");
+});
